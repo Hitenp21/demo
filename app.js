dca235a8451dfb35b9a81e8c3197dfca697881a5
Hitenp21 <hiten.m@pluto-men.com>
2024-05-22 13:08:21 +0530

    commited main

--- app.js
+++ app.js
@@ -2,3 +2,11 @@
 console.log("hello");
 
 //add new feature in main
+<<<<<<< Updated upstream
+
+// Add new owsome fetutre in main branch
+
+// git stash push 3
+=======
+// abc
+>>>>>>> Stashed changes
