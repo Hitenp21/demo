a65e2ec6413caf20bcc3bfb7dc2639145fe270ce
Hitenp21 <hiten.m@pluto-men.com>
2024-05-22 13:29:08 +0530

    old

--- app.js
+++ app.js
@@ -3,12 +3,8 @@ console.log("hello");
 
 //add newfeature
 
-<<<<<<< Updated upstream
-// additional 
-=======
 //additional
 
 function abc(){
     console.log("object")
 }
->>>>>>> Stashed changes
